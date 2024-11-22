1d51c54edd3a9c36d2cf4e05be0d02fb0ec245a9
Nick Verges <nverges@email.arizona.edu>
2024-11-21 22:14:35 -0700

    Fix logic

--- src/prompts/import_pivotal_estimates.mjs
+++ src/prompts/import_pivotal_estimates.mjs
@@ -21,24 +21,36 @@ async function importPivotalEstimates({ teamId }) {
   if (importEstimates) {
     const estimateData = await fetchEstimatesForTeam({ teamId });
 
-    if (!estimateData.type) {
-      console.log(chalk.yellow('⚠️  No estimation type found. Please set one.'));
-    }
+    // if (estimateData.type) {
+      const { changeEstimateType } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "changeEstimateType",
+          choices: [
+            { name: "Yes", value: true },
+            { name: "No", value: false },
+          ],
+          message: `Your estimation scale is already set to ${estimateData.type} (${ESTIMATION_SCALES[estimateData.type]}).\n  Pivotal estimates will be rounded to the nearest value. Change it?`, 
+          default: false,
+        },
+      ]);
 
-    const { estimationScale } = await inquirer.prompt([
-      {
-        type: "list",
-        name: "estimationScale",
-        message: estimateData.type 
-          ? `Your estimation scale is currently ${estimateData.type} (${ESTIMATION_SCALES[estimateData.type]}). Select a new scale:`
-          : "Select an estimation scale:",
-        choices: [
-          { name: "Exponential (0,1,2,4,8,16)", value: "exponential" },
-          { name: "Fibonacci (0,1,2,3,5,8)", value: "fibonacci" },
-          { name: "Linear (0,1,2,3,4,5)", value: "linear" },
-        ],
-      },
-    ]);
+      if (!changeEstimateType) {
+        return estimateData.type;
+      }
+
+      const { estimationScale } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "estimationScale",
+          message: "Select an estimation scale:",
+          choices: [
+            { name: "Exponential (0,1,2,4,8,16)", value: "exponential" },
+            { name: "Fibonacci (0,1,2,3,5,8)", value: "fibonacci" },
+            { name: "Linear (0,1,2,3,4,5)", value: "linear" },
+          ],
+        },
+      ]);
 
     await linearClient.updateTeam(teamId, {
       issueEstimationType: estimationScale,
