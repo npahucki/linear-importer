b3ec0f7a3bc58a7da44865740fe3bf18a313c632
Nick Verges <nverges@email.arizona.edu>
2024-11-21 22:22:01 -0700

    Formatting

--- src/prompts/import_pivotal_estimates.mjs
+++ src/prompts/import_pivotal_estimates.mjs
@@ -22,35 +22,39 @@ async function importPivotalEstimates({ teamId }) {
     const estimateData = await fetchEstimatesForTeam({ teamId });
 
     // if (estimateData.type) {
-      const { changeEstimateType } = await inquirer.prompt([
-        {
-          type: "list",
-          name: "changeEstimateType",
-          choices: [
-            { name: "Yes", value: true },
-            { name: "No", value: false },
-          ],
-          message: `Your estimation scale is already set to ${estimateData.type} (${ESTIMATION_SCALES[estimateData.type]}).\n  Pivotal estimates will be rounded to the nearest value. Change it?`, 
-          default: false,
-        },
-      ]);
+    const { changeEstimateType } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "changeEstimateType",
+        choices: [
+          { name: "Yes", value: true },
+          { name: "No", value: false },
+        ],
+        message: `Your estimation scale is already set to ${
+          estimateData.type
+        } (${
+          ESTIMATION_SCALES[estimateData.type]
+        }).\n  Pivotal estimates will be rounded to the nearest value. Change it?`,
+        default: false,
+      },
+    ]);
 
-      if (!changeEstimateType) {
-        return estimateData.type;
-      }
+    if (!changeEstimateType) {
+      return estimateData.type;
+    }
 
-      const { estimationScale } = await inquirer.prompt([
-        {
-          type: "list",
-          name: "estimationScale",
-          message: "Select an estimation scale:",
-          choices: [
-            { name: "Exponential (0,1,2,4,8,16)", value: "exponential" },
-            { name: "Fibonacci (0,1,2,3,5,8)", value: "fibonacci" },
-            { name: "Linear (0,1,2,3,4,5)", value: "linear" },
-          ],
-        },
-      ]);
+    const { estimationScale } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "estimationScale",
+        message: "Select an estimation scale:",
+        choices: [
+          { name: "Exponential (0,1,2,4,8,16)", value: "exponential" },
+          { name: "Fibonacci (0,1,2,3,5,8)", value: "fibonacci" },
+          { name: "Linear (0,1,2,3,4,5)", value: "linear" },
+        ],
+      },
+    ]);
 
     await linearClient.updateTeam(teamId, {
       issueEstimationType: estimationScale,
